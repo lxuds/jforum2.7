5641424e56c0420b51caa339c60fe9934ba8dec0
udittmer <udittmer@a05e8a62-9b3a-4cc5-b6cd-d60128068f0b>
2018-03-28 12:43:14 +0000

    more FreeMarker 2.3.27 compatibility

--- trunk/src/main/java/net/jforum/JForumBaseServlet.java
+++ trunk/src/main/java/net/jforum/JForumBaseServlet.java
@@ -89,7 +89,7 @@ public class JForumBaseServlet extends HttpServlet
 			ConfigLoader.startCacheEngine();
 
 			// Configure the template engine
-			final Configuration templateCfg = new Configuration(Configuration.VERSION_2_3_23);
+			final Configuration templateCfg = new Configuration(Configuration.VERSION_2_3_27);
 			if (this.debug) {
 				templateCfg.setTemplateUpdateDelayMilliseconds(2000);
 			} else {
